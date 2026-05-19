6cefea39459ecb29dd782b3aab84a5e09d2e27c4
beluga11716 <long20071117@gmail.com>
2026-05-19 21:50:10 +0800

    加了音乐嘻嘻

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -68,11 +68,11 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件类型：音乐播放器
 			type: "music",
 			// 是否启用该组件
-			enable: false,
+			enable: true,
 			// 组件位置
 			position: "sticky",
 			// 是否在文章详情页显示
-			showOnPostPage: false,
+			showOnPostPage: true,
 		},
 		{
 			// 组件类型：分类组件
